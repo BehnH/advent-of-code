d37f159b6553ca8cb12c54c4e479ba70f47befc7
Behn Hayhoe <7383025+BehnH@users.noreply.github.com>
2022-12-06 15:57:34 +0000

    yr-2022/day-06 complete 🎉

---
+++ yr-2022/day-06/day-06_a.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func isUnique(s string) bool {
+	seen := map[rune]bool{}
+	for _, c := range s {
+		seen[c] = true
+	}
+	return len(seen) == len(s)
+}
+
+func main()  {
+	// Read input
+	input, err := os.Open("input.txt")
+
+	// Panic if the input file could not be opened
+	if err != nil {
+		panic(err)
+	}
+
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+	sc.Scan()
+
+	chars := sc.Text()
+
+	n1 := 0
+
+	// Write a comment which explains the logic of the following code
+	for i := 3; i < len(chars); i++ {
+		if isUnique(chars[i-3:i+1]) && n1 == 0 {
+			n1 = i + 1
+		}
+	}
+
+	// Print the result
+	fmt.Println(n1)
+
+}
