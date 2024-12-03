978285946c9a97fea641406c885a74a520639e76
Behn <7383025+BehnH@users.noreply.github.com>
2024-12-03 10:06:45 +0000

    Update template

--- .tmpl/main.go
+++ .tmpl/main.go
@@ -1,21 +1,41 @@
 package main
 
-import "github.com/behnh/advent-of-code/pkg/utils"
+import (
+	"fmt"
 
-func main()
+	"github.com/behnh/advent-of-code/pkg/utils"
+)
 
-func partOne() {
-	lines, err := utils.GetLines("input.txt")
-	if err != nil {
-		return 0
-	}
+func main() {
+	answer1 := partOne()
+	answer2 := partTwo()
 
+	fmt.Println("Part 1:", answer1)
+	fmt.Println("Part 2:", answer2)
 }
 
-func partTwo() {
+func partOne() int {
 	lines, err := utils.GetLines("input.txt")
 	if err != nil {
 		return 0
 	}
 
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	return 0
+}
+
+func partTwo() int {
+	lines, err := utils.GetLines("input.txt")
+	if err != nil {
+		return 0
+	}
+
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	return 0
 }
